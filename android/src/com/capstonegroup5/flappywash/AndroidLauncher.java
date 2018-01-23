47eef7b001b84dabf0cc0e5ed04b6f35f591f275
Andrew Beers <apbeers@gmail.com>
2018-01-23 17:09:38 -0600

    initial commit

---
+++ android/src/com/capstonegroup5/flappywash/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.capstonegroup5.flappywash;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.capstonegroup5.flappywash.FlappyWash;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyWash(), config);
+	}
+}
