28254f17b749aa88672b76ea611170fc06114341
Andrew Beers <apbeers@gmail.com>
2018-01-23 19:13:33 -0600

    video4

--- FlappyWash/core/src/com/capstonegroup5/flappywash/States/MenuState.java
+++ FlappyWash/core/src/com/capstonegroup5/flappywash/States/MenuState.java
@@ -1,6 +1,8 @@
 package com.capstonegroup5.flappywash.States;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.capstonegroup5.flappywash.FlappyWash;
 
 /**
  * Created by andrew on 1/23/18.
@@ -8,8 +10,13 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MenuState extends State {
 
+    private Texture background;
+    private Texture playBtn;
+
     public MenuState(GameStateManager gsm) {
         super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playBtn.png");
     }
 
     @Override
@@ -25,5 +32,9 @@ public class MenuState extends State {
     @Override
     public void render(SpriteBatch sb) {
 
+        sb.begin();
+        sb.draw(background, 0,0, FlappyWash.WIDTH, FlappyWash.HEIGHT);
+        sb.draw(playBtn, FlappyWash.WIDTH/2 - playBtn.getWidth()/2, FlappyWash.HEIGHT/2 - playBtn.getHeight()/2);
+        sb.end();
     }
 }
