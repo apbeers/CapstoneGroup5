226edea16ea0763840c0eb07612af8d37adf7976
awinebre <awinebre@email.uark.edu>
2018-02-03 16:14:20 -0600

    Video #8 Completion

--- core/src/com/capstonegroup5/flappywash/states/PlayState.java
+++ core/src/com/capstonegroup5/flappywash/states/PlayState.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.capstonegroup5.flappywash.FlappyWash;
 import com.capstonegroup5.flappywash.sprite.Bird;
+import com.capstonegroup5.flappywash.sprite.Tube;
 
 /**
  * Created by andrew on 2/1/18.
@@ -13,12 +14,14 @@ import com.capstonegroup5.flappywash.sprite.Bird;
 public class PlayState extends State {
     private Bird bird;
     private Texture bg;
+    private Tube tube;
 
     protected PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 300);
         cam.setToOrtho(false, FlappyWash.WIDTH/2, FlappyWash.HEIGHT/2);
         bg = new Texture("bg.png");
+        tube = new Tube(100);
     }
 
     @Override
@@ -40,6 +43,8 @@ public class PlayState extends State {
         sb.begin();
         sb.draw(bg, cam.position.x - (cam.viewportWidth /2),0);
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+        sb.draw(tube.getTopTube(),tube.getPosTopTube().x, tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
         sb.end();
     }
 
